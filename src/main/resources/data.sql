1a80a984cea16cd18fb9864e493ddbfa9666b2d5
David-hub777 <david25042001@gmail.com>
2023-11-12 21:51:09 +0100

    segundo commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into student values(10001,'Ragna', 'Pasaporte1');
+insert into student values(10002,'Ivra', 'Pasaporte2');
---
+++ src/test/java/com/example/app/con/springboot/AppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app.con.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
